2c7d7b8ade3d42877ac2c5429b7e25e339d9c8de
lee2886 <k.lee.rv@future.co.jp>
2020-11-10 22:45:06 +0900

    secured by keycloak with PKCE

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Keycloak from 'keycloak-js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const config: Keycloak.KeycloakConfig = {
+  url: "http://localhost:8888/auth",
+  realm: "backoffice",
+  clientId: "billing"
+};
+
+const keycloak = Keycloak(config);
+
+keycloak.init({onLoad: "login-required", pkceMethod: "S256"}).then((auth: boolean) => {
+  if (!auth) {
+    window.location.reload();
+  }
+
+  console.log(keycloak);
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
