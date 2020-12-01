abc3b5dc0b0075e7abf25a79c656a2064de13a9a
lee2886 <k.lee.rv@future.co.jp>
2020-12-01 12:06:47 +0900

    Parsed id token interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { KeycloakInstance } from 'keycloak-js';
+import { KeycloakInstance, KeycloakTokenParsed } from 'keycloak-js';
 
-const App: React.FC<{keycloak: KeycloakInstance}> = ({ keycloak }) => {
+interface IdTokenParsed extends KeycloakTokenParsed {
+  name?: string;
+  preferred_username?: string;
+  email?: string;
+}
+
+interface KcInstance extends KeycloakInstance {
+  idTokenParsed?: IdTokenParsed;
+}
+
+const App: React.FC<{keycloak: KcInstance}> = ({ keycloak }) => {
+  // useEffect(() => {
+  //   keycloak.loadUserProfile().then((profile: KeycloakProfile) => {
+  //     console.log(profile);
+  //   });
+  // });
   return (
     <div className="App">
       <header className="App-header">
+        <p>{keycloak.idTokenParsed?.preferred_username}</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
